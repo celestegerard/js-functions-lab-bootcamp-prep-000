a0988d665d6a471b473cd5954e22c9447af1bd2a
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-19 01:13:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(halloween, Mom) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
