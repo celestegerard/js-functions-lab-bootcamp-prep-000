609334610104934ce7a6414b66f118ee78e5ff91
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-19 01:10:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+
+function happyHolidays() {
+  return "Happy Holidays!"
+}
+
+function happyHolidaysTo(Mom) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidaysTo(halloween, Mom) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(halloween, days) {
+  return `It's ${days} days until ${holiday}!`
+}
